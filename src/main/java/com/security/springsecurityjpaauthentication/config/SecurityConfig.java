5a417f21034c4af5dbc17c78c286c7b669c280ab
Thiago Hernandes <943488@renner.onmicrosoft.com>
2022-05-13 09:49:07 -0300

    Filter security add

--- src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.security.springsecurityjpaauthentication.config;
 
+import com.security.springsecurityjpaauthentication.filter.AuthenticationLoggingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,7 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.addFilterAfter(
+            new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+            .authorizeRequests()
             .anyRequest().authenticated()
             .and()
             .formLogin()
