a7d9a7076ecaf5c223aaf9eae263117450f1debd
Thiago Hernandes <943488@renner.onmicrosoft.com>
2022-05-20 11:28:24 -0300

    HASAUTHORITY add

--- src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+            .antMatchers("/teste").hasAuthority("TESTE")
             .anyRequest().authenticated()
             .and()
             .formLogin()
--- src/main/java/com/security/springsecurityjpaauthentication/controller/LoginController.java
+++ src/main/java/com/security/springsecurityjpaauthentication/controller/LoginController.java
@@ -8,6 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    @GetMapping("/teste")
+    public String teste(Authentication a, Model model) {
+        model.addAttribute("username", a.getName());
+        return "teste";
+    }
+
+    @GetMapping("/teste2")
+    public String teste2(Authentication a, Model model) {
+        model.addAttribute("username", a.getName());
+        return "teste2";
+    }
+
     @GetMapping("/index")
     public String index(Authentication a, Model model) {
         model.addAttribute("username", a.getName());
