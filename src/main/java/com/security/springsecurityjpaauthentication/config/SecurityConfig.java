54fe52427e672d3a1061970c39a477ab2af87b00
Thiago Hernandes <943488@renner.onmicrosoft.com>
2022-05-20 13:16:19 -0300

    Filter Security

--- src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurityjpaauthentication/config/SecurityConfig.java
@@ -18,7 +18,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.addFilterAfter(
+                new AuthenticationLoggingFilter(),
+                BasicAuthenticationFilter.class)
+            .authorizeRequests()
             .antMatchers("/teste").hasAuthority("TESTE")
             .anyRequest().authenticated()
             .and()
